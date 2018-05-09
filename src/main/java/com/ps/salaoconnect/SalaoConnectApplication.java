857f5b2665e4992f1974282732d8804f63f76537
thiagouft <thiagoufma@gmail.com>
2018-05-08 21:30:20 -0300

    Projeto Criado

---
+++ src/main/java/com/ps/salaoconnect/SalaoConnectApplication.java
@@ -0,0 +1,12 @@
+package com.ps.salaoconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaoConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaoConnectApplication.class, args);
+	}
+}
